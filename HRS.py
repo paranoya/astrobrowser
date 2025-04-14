53679ab8db8d25cadd123c0e09ca777cbac20f2e
Yago Ascasibar <yago.ascasibar@uam.es>
2025-04-14 12:32:37 +0200

    clean-up

--- HRS.py
+++ HRS.py
@@ -33,8 +33,8 @@ from scripts import astrobrowser
 # In[ ]:
 
 
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
+#get_ipython().run_line_magic('load_ext', 'autoreload')
+#get_ipython().run_line_magic('autoreload', '2')
 
 
 # Utility functions
